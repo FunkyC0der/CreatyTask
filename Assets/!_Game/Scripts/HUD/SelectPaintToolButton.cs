f2c95b393bff650aec7db49b9c99cf28d25b4163
funky_coder <e.ruksov@gmail.com>
2024-08-21 09:09:09 +0300

    Add button to select paint tool * Add fill paint tool

---
+++ Assets/!_Game/Scripts/HUD/SelectPaintToolButton.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CreatyTest.HUD
+{
+  [RequireComponent(typeof(Button))]
+  public class SelectPaintToolButton : MonoBehaviour
+  {
+    public PaintService PaintService;
+    public PaintToolDesc PaintToolDesc;
+
+    private Button m_button;
+
+    private void Awake()
+    {
+      m_button = GetComponent<Button>();
+      m_button.onClick.AddListener(SelectPaintTool);
+    }
+
+    private void Start()
+    {
+      UpdateView();
+      PaintService.OnPaintToolChanged += UpdateView;
+    }
+
+    private void UpdateView() => 
+      m_button.interactable = PaintToolDesc != PaintService.PaintToolDesc;
+
+    private void SelectPaintTool() => 
+      PaintService.ChangePaintTool(PaintToolDesc);
+  }
+}
